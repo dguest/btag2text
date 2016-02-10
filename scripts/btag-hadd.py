9507b2d6ccce4b58ca817f4de833e58e341a2742
Dan Guest <dguest@cern.ch>
2016-02-10 12:54:21 +0100

    Add verbose option

--- scripts/btag-hadd.py
+++ scripts/btag-hadd.py
@@ -7,11 +7,12 @@ def _get_args():
     parser = ArgumentParser(description=__doc__)
     parser.add_argument("in_files", nargs="+")
     parser.add_argument('-o', "--out-file", required=True)
+    parser.add_argument('-v', '--verbose', action='store_true')
     return parser.parse_args()
 
 def run():
     args = _get_args()
-    hadd(args.out_file, args.in_files)
+    hadd(args.out_file, args.in_files, verbose=args.verbose)
 
 if __name__ == '__main__':
     run()
