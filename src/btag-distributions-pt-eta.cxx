70afee1b1d39f143bdd13633e348bf4a932a2299
Dan Guest <dguest@cern.ch>
2016-06-23 17:53:17 +0200

    find jet collection automatically

--- src/btag-distributions-pt-eta.cxx
+++ src/btag-distributions-pt-eta.cxx
@@ -1,5 +1,6 @@
 #include "FileCLI.hh"
 #include "constants.hh"
+#include "get_tree.hh"
 
 #include "ndhist/Histogram.hh"
 
@@ -59,7 +60,7 @@ int main(int argc, char* argv[]) {
   FileCLI cli(argc, argv);
 
   // setup chain
-  TChain chain("bTag_AntiKt4EMTopoJets");
+  TChain chain(get_tree(cli.in_files().at(0)).c_str());
   for (const auto& in: cli.in_files()) {
     chain.Add(in.c_str());
   }
