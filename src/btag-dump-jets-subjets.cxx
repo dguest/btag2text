40adfaa1337cc87d35fb731e9db6298cb790c5b9
Dan Guest <dguest@cern.ch>
2016-08-19 19:21:29 +0200

    adapt the subjet dumper to use boost program options

--- src/btag-dump-jets-subjets.cxx
+++ src/btag-dump-jets-subjets.cxx
@@ -1,21 +1,23 @@
-#include "FileCLI.hh"
+#include "Options.hh"
 #include "Jets.hh"
 #include "SmartChain.hh"
-#include "FlavorPtEtaDistributions.hh"
+// #include "FlavorPtEtaDistributions.hh"
 #include "output_templates.hh"
 #include "constants.hh"
 #include "unshittify.hh"
 #include "select_jet.hh"
+#include "get_tree.hh"
 
 #include "H5Cpp.h"
 
 #include <iostream>
 #include <limits>
 
-// const double GeV = 1000;
-// const double MAX_PT = 1000*GeV;
-
-std::string help = "Dump IP3D information for jets";
+const std::string DESCRIPTION = (
+  "Dump information for jets\n"
+  "Should be pretty comprehensive, including "
+  "subjets, tracks, and clusters"
+  );
 
 // _____________________________________________________________________
 // main function
@@ -26,18 +28,13 @@ int main(int argc, char* argv[]) {
   H5::Exception::dontPrint();
 
   // load info
-  FileCLI cli(argc, argv, Output::NO, help);
-  SmartChain chain(JET_COLLECTION);
-  for (const auto& in: cli.in_files()) {
+  const auto opts = get_opts(argc, argv, DESCRIPTION);
+  SmartChain chain(get_tree(opts.input_files.at(0)));
+  for (const auto& in: opts.input_files) {
     chain.add(in);
   }
   Jets jets(chain);
   int n_entries = chain.GetEntries();
-
-  // load distributions
-  require(REWEIGHT_FILE);
-  FlavorPtEtaDistributions pt_eta_reweight(
-    H5::H5File(REWEIGHT_FILE, H5F_ACC_RDONLY));
 
   for (int iii = 0; iii < n_entries; iii++) {
     chain.GetEntry(iii);
@@ -45,9 +42,7 @@ int main(int argc, char* argv[]) {
     for (int jjj = 0; jjj < n_jets; jjj++) {
       auto jet = jets.getJet(jjj);
       if (! select_jet(jet) ) continue;
-      auto pt_eta = get_pt_eta_map(jet);
-      double weight = pt_eta_reweight.get(pt_eta, jet.jet_truthflav);
-      std::cout << str_from_fat_jet(jet, weight) << std::endl;
+      std::cout << str_from_fat_jet(jet, opts.weight) << std::endl;
     }
   }
 }
