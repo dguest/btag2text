8d449e14ee05d25d38b3bf6abf5d112c1b1ea069
Dan Guest <dguest@cern.ch>
2016-08-24 18:37:40 +0200

    update metadata collector to aggregate sum_event_weights

--- scripts/btag-metadata-collect.py
+++ scripts/btag-metadata-collect.py
@@ -24,7 +24,7 @@ def run():
 
     # base entry for metadata
     def base_meta():
-        return {'n_events': 0}
+        return {'n_events': 0, 'sum_event_weights': 0.0}
     metadict = defaultdict(base_meta)
     for ds, files in datasets.items():
         if args.verbose:
@@ -33,10 +33,11 @@ def run():
             with File(h5name,'r') as h5file:
                 entries = int(h5file.attrs['n_entries'])
                 metadict[ds]['n_events'] += entries
+                for attr in ['sum_event_weights']:
+                    metadict[ds][attr] += h5file.attrs[attr]
 
     with open(args.output_file, 'w') as outfile:
         json_metadict = {str(k): v for k, v in metadict.items()}
-        print(json_metadict)
         outfile.write(json.dumps(json_metadict, indent=2))
 
 if __name__ == '__main__':
