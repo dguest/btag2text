d5f83871e3dd859ca8db3f3f9f23473e6659b7c3
Dan Guest <dguest@cern.ch>
2016-08-29 12:44:39 +0200

    update text file format for fat jets

--- src/btag-dump-jets-fatjets.cxx
+++ src/btag-dump-jets-fatjets.cxx
@@ -41,7 +41,7 @@ int main(int argc, char* argv[]) {
     int n_jets = jets.size();
     for (int jjj = 0; jjj < n_jets; jjj++) {
       auto jet = jets.getJet(jjj);
-      if (! select_jet(jet) ) continue;
+      if (! select_fat_jet(jet) ) continue;
       std::cout << str_from_fat_jet(jet, opts.weight) << std::endl;
     }
   }
