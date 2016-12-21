74ccb3f5d96bfefd8ec9638ef412c8a50cc07ecf
Dan Guest <dguest@cern.ch>
2016-12-21 22:52:16 +0100

    Extend TTree name heuristic to remote files
    No longer check if a file exists if it looks like a remote file that
    only ROOT knows how to open. Instead we now use the TFile::Open class
    method and check more carefully to make sure it's been opened.

--- src/get_tree.cxx
+++ src/get_tree.cxx
@@ -4,10 +4,19 @@
 #include <stdexcept>
 #include <set>
 #include <fstream>
+#include <regex>
+#include <memory>
 
 #include "TKey.h"
 
 namespace {
+  bool is_remote(const std::string& file_name) {
+    std::regex remote_check("[a-z]+:.*");
+    if (std::regex_match(file_name, remote_check)) {
+      return true;
+    }
+    return false;
+  }
   bool exists(const std::string& file_name) {
     std::ifstream file(file_name.c_str(), std::ios::binary);
     if (!file) {
@@ -20,14 +29,17 @@ namespace {
 }
 
 std::string get_tree(const std::string& file_name) {
-  if (!exists(file_name)) {
+  if (!exists(file_name) && !is_remote(file_name)) {
     throw std::logic_error(file_name + " doesn't exist");
   }
-  TFile file(file_name.c_str());
+  std::unique_ptr<TFile> file(TFile::Open(file_name.c_str()));
+  if (!file || !file->IsOpen() || file->IsZombie()) {
+    throw std::logic_error("can't open " + file_name);
+  }
   std::set<std::string> keys;
-  int n_keys = file.GetListOfKeys()->GetSize();
+  int n_keys = file->GetListOfKeys()->GetSize();
   for (int keyn = 0; keyn < n_keys; keyn++) {
-    keys.insert(file.GetListOfKeys()->At(0)->GetName());
+    keys.insert(file->GetListOfKeys()->At(0)->GetName());
   }
   size_t n_unique = keys.size();
   if (n_unique > 1) {
@@ -42,7 +54,9 @@ std::string get_tree(const std::string& file_name) {
     prob.append("]");
     throw std::logic_error(prob);
   }
-  auto* key = dynamic_cast<TKey*>(file.GetListOfKeys()->At(0));
-  return key->GetName();
+  auto* key = dynamic_cast<TKey*>(file->GetListOfKeys()->At(0));
+  std::string name = key->GetName();
+  file->Close();
+  return name;
 }
 
