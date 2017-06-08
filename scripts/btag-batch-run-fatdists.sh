27e000b8e3f85c18577b6f9ed32ffac50637d6f2
Daniel Guest <dguest@cern.ch>
2017-06-08 14:15:18 -0700

    make reweighting file manditory for fat dists

--- scripts/btag-batch-run-fatdists.sh
+++ scripts/btag-batch-run-fatdists.sh
@@ -23,9 +23,15 @@ OUTPUT_FILE=$(get-output-name $INPUT_FILE)
 echo "running on $INPUT_FILE"
 mkdir -p $OUTPUT_DIR
 OUTPUT_PATH=${OUTPUT_DIR}/$OUTPUT_FILE.h5
+
 XSEC_FILE=$PY_DIR/../data/xsec.txt
 if [[ ! -f $XSEC_FILE ]] ; then
     echo "ERROR: no xsec file found" >&2
+    exit 1
+fi
+RW_FILE=reweight.h5
+if [[ ! -f $RW_FILE ]] ; then
+    echo "ERROR: no reweighting file found!" >&2
     exit 1
 fi
 DSID=$(echo $OUTPUT_FILE | sed -r 's/d([0-9]*).*/\1/')
@@ -34,5 +40,5 @@ if ! WEIGHT=$(echo $DSID | $PY_DIR/btag-get-xsec.py $XSEC_FILE); then
     exit 1
 fi
 echo "weighted by $WEIGHT"
-btag-distributions-fatjets $INPUT_FILE -o $OUTPUT_PATH -w $WEIGHT
+btag-distributions-fatjets $INPUT_FILE -o $OUTPUT_PATH -w $WEIGHT -r $RW_FILE
 
